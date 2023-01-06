4c8bb53c2cb4042369a88d9499eeb4f3d1deb637
matttttttso <u2.pinetail@gmail.com>
2023-01-06 13:51:34 +0900

    initial commit

---
+++ gradle-test/src/main/java/gradletest/GradleTestApplication.java
@@ -0,0 +1,15 @@
+package gradletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleTestApplication.class, args);
+		
+		System.out.println("test");
+	}
+
+}
---
+++ gradle-test/src/test/java/gradletest/GradleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package gradletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
