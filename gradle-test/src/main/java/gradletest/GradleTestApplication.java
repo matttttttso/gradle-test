55e6bbb8ae800793a2336deb590d0e8d078e4821
matttttttso <u2.pinetail@gmail.com>
2023-09-26 17:08:23 +0900

    9/26

--- gradle-test/src/main/java/gradletest/GradleTestApplication.java
+++ gradle-test/src/main/java/gradletest/GradleTestApplication.java
@@ -1,5 +1,7 @@
 package gradletest;
 
+import java.util.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +12,13 @@ public class GradleTestApplication {
 		SpringApplication.run(GradleTestApplication.class, args);
 		
 		System.out.println("test");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		String s = sc.next();
+		System.out.println((a+b+c) + " " + s);
+		sc.close();
 	}
 
 }
